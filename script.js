dff02dd078557580ac32748c2011fdff8dd69ff6
Gabi Viana <gabigviana@gmail.com>
2020-02-27 22:14:11 +0100

    blog page + active li burger menu

--- script.js
+++ script.js
@@ -8,7 +8,26 @@ function toggleMenu(e) {
     e.preventDefault();
 }
 
-window.addEventListener("DOMContentLoaded", function() {
+window.addEventListener("DOMContentLoaded", function () {
     document.querySelector(".burger-menu__toolbar a").addEventListener("click", toggleMenu);
     document.querySelector(".burger-menu-icon a").addEventListener("click", toggleMenu);
 });
+
+
+
+function readFunction(e) {
+    var Btn = e.target;
+    var blogText = Btn.closest(".blogText");
+    var dots = blogText.querySelector(".dots");
+    var moreText = blogText.querySelector(".more");
+
+    if (dots.style.display === "none") {
+        dots.style.display = "inline";
+        Btn.innerHTML = "Read more";
+        moreText.style.display = "none";
+    } else {
+        dots.style.display = "none";
+        Btn.innerHTML = "Read less";
+        moreText.style.display = "inline";
+    }
+}

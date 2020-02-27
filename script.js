4fb835112ed7f8310cc07be58a662698832cfd24
HappyMaddMan <crisan_ciprian_vasile@yahoo.com>
2020-02-27 12:42:05 +0100

    text & image creation

--- script.js
+++ script.js
@@ -15,4 +15,33 @@ function handleData(data){
 function showData(singleRowData){
 	// console.log("singleRowData - console")
 	// console.log(singleRowData.gsx$id.$t);
+	
+
+	// if (singleRowData.gsx$wheelsizeinches.$t == 26) {
+	// 	console.log(singleRowData.gsx$brand);
+	// }
+
+	const template = document.querySelector("template").content;
+	const clone = template.cloneNode(true);
+	const brand = clone.querySelector(".brand");
+	const name = clone.querySelector(".name");
+	const bike_img = clone.querySelector(".bike_img");
+	brand.textContent = singleRowData.gsx$brand.$t;
+	name.textContent = singleRowData.gsx$name.$t;
+
+	bike_img.alt = singleRowData.gsx$image.$t;
+
+	singleRowData.gsx$image.$t = "img/" + singleRowData.gsx$image.$t + ".png";
+
+	console.log(singleRowData.gsx$image.$t)
+
+	if (singleRowData.gsx$image.$t == "img/.png") {
+		bike_img.src = "https://via.placeholder.com/300x100";
+	}
+	else{
+		bike_img.src = singleRowData.gsx$image.$t;
+	}
+
+
+	document.querySelector(".container").appendChild(clone);
 }
